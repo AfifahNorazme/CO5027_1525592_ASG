840503cd59331d86d49c32008eae0091c344682d
AfifahNorazme <1525592@chester.ac.uk>
2017-04-27 20:17:20 +0800

    update

--- Grande Collecte/MapJavascript.js
+++ Grande Collecte/MapJavascript.js
@@ -1,7 +1,7 @@
 ﻿function initMap() {
     var lcb2017 = { lat: 4.885470, lng: 114.931614 };
     var map = new google.maps.Map(document.getElementById('gcmap'), {
-        zoom: 15,
+        zoom: 20,
         center: lcb2017
     });
     var marker = new google.maps.Marker({
